deff832f004a835fc78277d364f2f191feeed0d7
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-05 10:53:09 -0600

    Adjust iteration 3 and complete through all turns taken, debug ships separation b/w player & computer_board
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -69,18 +69,18 @@ class BoardTest < Minitest::Test
     assert @board.valid_placement?(@submarine, ["B1", "C1"])
   end
 
-  # def test_render
-  #   @board.place(@cruiser, ["A1", "A2", "A3"])
-  #   @board.board_render
-  #   @board.board_render
-  #   @board.cells["A1"].fire_upon
-  #   @board.board_render
-  #   @board.cells["A2"].fire_upon
-  #   @board.cells["A3"].fire_upon
-  #   @board.board_render
-  #   @board.cells["D3"].fire_upon
-  #   @board.board_render
-  # end
+  def test_render
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    @board.board_render
+    @board.board_render
+    @board.cells["A1"].fire_upon
+    @board.board_render
+    @board.cells["A2"].fire_upon
+    @board.cells["A3"].fire_upon
+    @board.board_render
+    @board.cells["D3"].fire_upon
+    @board.board_render
+  end
 
   def test_place_computer_ship
     @computer_board.computer_place(@cruiser)
