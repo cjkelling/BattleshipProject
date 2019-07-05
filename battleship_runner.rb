ffaf8d916c363491388c269ea20bb0237f2d5315
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-05 10:53:09 -0600

    Start turn_test file
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -57,7 +57,7 @@ end
 
 def print_results(results)
   if results == "player"
-    puts "You sunk the computer's last ship. Player won!"
+    puts "You sunk the computer's last ship. You win!"
   elsif results == "computer"
     puts "The computer sunk your last ship. Computer wins!"
   end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,15 +8,24 @@ require 'pry'
 
 class TurnTest < Minitest::Test
   def setup
-    @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
     @computer_board = Board.new
     @player_board = Board.new
+
+    @ships_computer = []
+    @ships_player = []
+    @ships_computer << @cruiser = Ship.new("Cruiser", 3)
+    @ships_computer << @submarine = Ship.new("Submarine", 2)
+    @ships_player << @cruiser = Ship.new("Cruiser", 3)
+    @ships_player << @submarine = Ship.new("Submarine", 2)
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @computer_board
+    assert_instance_of Board, @player_board
   end
 
   def test_display_boards
-
+    assert_equal
   end
 
 end
