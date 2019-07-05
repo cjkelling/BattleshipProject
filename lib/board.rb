8678aa964a5f3718fd3c79377335640adeb279f8
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-05 10:53:09 -0600

    Update branch to origin master
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -6,11 +6,7 @@ class Board
       "A1" => Cell.new("A1"), "A2" => Cell.new("A2"), "A3" => Cell.new("A3"), "A4" => Cell.new("A4"), +
       "B1" => Cell.new("B1"), "B2" => Cell.new("B2"), "B3" => Cell.new("B3"), "B4" => Cell.new("B4"), +
       "C1" => Cell.new("C1"), "C2" => Cell.new("C2"), "C3" => Cell.new("C3"), "C4" => Cell.new("C4"), +
-<<<<<<< HEAD
-      "D1" => Cell.new("D1"), "D2" => Cell.new("D2"), "D3" => Cell.new("D3"), "D4" => Cell.new("D4")
-=======
       "D1" => Cell.new("D1"), "D2" => Cell.new("D2"), "D3" => Cell.new("D3"), "D4" => Cell.new("D4"),
->>>>>>> 5ce3382841a441c953122635b2242bb915f134e6
     }
   end
 
@@ -67,14 +63,6 @@ class Board
   end
 
   def render(playing = false)
-<<<<<<< HEAD
-    if playing == true
-      "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
-      "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
-      "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
-      "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
-=======
     puts(
     "  1 2 3 4 \n" +
     "A . . . . \n" +
@@ -91,7 +79,6 @@ class Board
       "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
       "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
       )
->>>>>>> 5ce3382841a441c953122635b2242bb915f134e6
     end
   end
 end
