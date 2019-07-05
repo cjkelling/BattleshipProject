95e835291be490874d57eb9fbaabbb0d9738f116
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-05 14:51:13 -0600

    Render work
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,75 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells, :empty_board
 
   def initialize
-    @board = Hash.new
     @cells = {
-      'A1' => Cell.new('A1'),
-      'A2' => Cell.new('A2'),
-      'A3' => Cell.new('A3'),
-      'A4' => Cell.new('A4'),
-      'B1' => Cell.new('B1'),
-      'B2' => Cell.new('B2'),
-      'B3' => Cell.new('B3'),
-      'B4' => Cell.new('B4'),
-      'C1' => Cell.new('C1'),
-      'C2' => Cell.new('C2'),
-      'C3' => Cell.new('C3'),
-      'C4' => Cell.new('C4'),
-      'D1' => Cell.new('D1'),
-      'D2' => Cell.new('D2'),
-      'D3' => Cell.new('D3'),
-      'D4' => Cell.new('D4'),
-      }
+      "A1" => Cell.new("A1"), "A2" => Cell.new("A2"), "A3" => Cell.new("A3"), "A4" => Cell.new("A4"), +
+      "B1" => Cell.new("B1"), "B2" => Cell.new("B2"), "B3" => Cell.new("B3"), "B4" => Cell.new("B4"), +
+      "C1" => Cell.new("C1"), "C2" => Cell.new("C2"), "C3" => Cell.new("C3"), "C4" => Cell.new("C4"), +
+      "D1" => Cell.new("D1"), "D2" => Cell.new("D2"), "D3" => Cell.new("D3"), "D4" => Cell.new("D4"),
+    }
   end
 
   def valid_coordinate?(input)
-    if @cells.keys.include?(input) == true
+    if @cells.keys.include?(input)
       true
+    end
+  end
+
+  def valid_placement?(ship, coordinates)
+    unless ship.length == coordinates.count
+      return false
+    end
+
+    array_of_empties = coordinates.map do |coord|
+      @cells[coord].empty?
+    end
+
+    if array_of_empties.include?(false)
+      return false
+    end
+
+    row_array = []
+    column_array = []
+
+    coordinates.each do |cooradate|
+      array = cooradate.chars
+      row_array << array[0].ord
+      column_array << array[1].to_i
+    end
+
+    row_array.sort!
+    column_array.sort!
+
+    if row_array.uniq.length == 1
+      return validate_array?(column_array)
+    end
+
+    if column_array.uniq.length == 1
+      return validate_array?(row_array)
+    end
+  end
+
+  def validate_array?(array)
+    start = array.first
+    range = (start..start + array.length - 1)
+    array == range.to_a
+  end
+
+  def place(ship, array_of_coordinates)
+    array_of_coordinates.each do |coordinate|
+      @cells[coordinate].place_ship(ship)
+    end
+  end
+
+  def renders(playing = false)
+    # require 'pry'; binding.pry
+    if playing = true
+      "  1 2 3 4 \n" +
+      "A #{@cell.render("A1")} #{@cell.render("A2")} #{@cell.render("A3")} #{@cell.render("A4")} \n" +
+      "B #{@cell.render("B1")} #{@cell.render("B2")} #{@cell.render("B3")} #{@cell.render("B4")} \n" +
+      "C #{@cell.render("C1")} #{@cell.render("C2")} #{@cell.render("C3")} #{@cell.render("C4")} \n" +
+      "D #{@cell.render("D1")} #{@cell.render("D2")} #{@cell.render("D3")} #{@cell.render("D4")} \n"
     end
   end
 
--- test/board_test.rb
+++ test/board_test.rb
@@ -9,11 +9,15 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
     assert_instance_of Board, @board
     assert_instance_of Hash, @board.cells
+    assert_instance_of Ship, @cruiser
+    assert_instance_of Ship, @submarine
   end
 
   def test_board_cells
@@ -21,8 +25,54 @@ class BoardTest < Minitest::Test
   end
 
   def test_valid_coordinate
-    assert_equal true, @board.valid_coordinate?('A1')
-    refute_equal true, @board.valid_coordinate?('D5')
+    assert @board.valid_coordinate?('A1')
+    assert @board.valid_coordinate?('D4')
+    refute @board.valid_coordinate?('A5')
+    refute @board.valid_coordinate?('E1')
+    refute @board.valid_coordinate?('A22')
+  end
+
+  def test_valid_length
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+    assert @board.valid_placement?(@submarine, ["C2", "C3"])
+  end
+
+  def test_coordinates_consecutive
+    refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(@submarine, ["A1", "C1"])
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@submarine, ["C2", "D3"])
+    assert @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_place_ship
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+
+    assert cell_1, cell_1.ship
+    assert cell_2, cell_2.ship
+    assert cell_3, cell_3.ship
+    assert cell_1.ship == cell_2.ship && cell_3.ship
+  end
+
+  def test_overlapping_ships
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    refute @board.valid_placement?(@submarine, ["A1", "B1"])
+    assert @board.valid_placement?(@submarine, ["B1", "C1"])
+  end
+
+  def test_renders
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    @board.renders
+
+    assert @board.renders
+    assert @board.renders(true)
   end
 
 end
